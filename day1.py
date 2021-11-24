062d9ef0b05c0d4daa8b767b30d59b28db7b66db
edevops <edevops@saviynt.com>
2021-11-24 17:41:52 -0500

    day1 done

---
+++ day1.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+
+city_name = input("What's name of the city you grew up in? ")
+
+pet_name = input("What is the name of your pet? ")
+
+print("Your band name is: " + city_name + pet_name)
